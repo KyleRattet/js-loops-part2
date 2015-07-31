a1e9976d2ad97cb5c908c7ab5877cfb86519a72d
KyleRattet <krattet@gmail.com>
2015-07-31 08:51:17 -0600

    solution to problem 2

--- js/main.js
+++ js/main.js
@@ -10,12 +10,16 @@ var students = ['joe', 'carl', 'rachel', 'derek', 'lily'];
 //   console.log(students[i]);
 // };
 
-//PROBLEM 1
-for (var i = 0; i < 4; i++) {
-  console.log(students[i]);
-};
+//PROBLEM 1 (LOG JOE, CARL, RACHEL, DEREK)
+// for (var i = 0; i < 4; i++) {
+//   console.log(students[i]);
+// }
 
-//
-
+//PROBLEM 2 (LOG JOE, RACHEL, LILLY)
+for (var i = 0; i < students.length; i++) {
+  if(i%2==0) {
+    console.log(students[i]);
+  }
+}
 
 });
