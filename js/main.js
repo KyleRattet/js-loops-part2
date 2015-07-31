41a9ce3fbfbab1d794236f90134e80a7b95b4510
KyleRattet <krattet@gmail.com>
2015-07-31 08:46:41 -0600

    solution to problem 1

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
 // add scripts
 
 $(document).on('ready', function() {
-  console.log('sanity check!');
+
+//global variables
+var students = ['joe', 'carl', 'rachel', 'derek', 'lily'];
+
+//Example
+// for (var i = 0; i < students.length; i++) {
+//   console.log(students[i]);
+// };
+
+//PROBLEM 1
+for (var i = 0; i < 4; i++) {
+  console.log(students[i]);
+};
+
+//
+
+
 });
